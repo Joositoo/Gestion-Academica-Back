24f1e7cc15e33ad82804ce2882c9656ad652715f
Joositoo <jmepk115@gmail.com>
2025-06-12 22:11:27 +0200

    Update webConfig

--- src/main/java/org/example/gestionAcademica/config/WebConfig.java
+++ src/main/java/org/example/gestionAcademica/config/WebConfig.java
@@ -1,15 +1,21 @@
 package org.example.gestionAcademica.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @PostConstruct
+    public void init() {
+        System.out.println("** CORS_DEBUG: CorsConfig Loaded! **");
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://gestionacademicafront.onrender.com")
+                .allowedOrigins("https://gestionacademicafront.onrender.com") //.allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
